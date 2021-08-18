cf8b25eff91800ce95d3b56cf937f5c119ad3d7b
Sunilg66 <sunilkg251@gmail.com>
2021-08-18 23:39:25 +0530

    reading data from excel

---
+++ palaceTask/src/com/sunil/poi/PalaceTester.java
@@ -0,0 +1,45 @@
+package com.sunil.poi;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class PalaceTester {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("C:\\Users\\sunil\\Documents//PalaceTable.xlsx"); // C:\Users\sunil\Documents
+			FileInputStream fis = new FileInputStream(file);
+
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+			XSSFSheet sheet = wb.getSheetAt(0);
+			Iterator<Row> itr = sheet.iterator();
+			while (itr.hasNext()) {
+				Row row = itr.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_STRING:
+						System.out.print(cell.getStringCellValue() + "\t\t\t");
+						break;
+					case Cell.CELL_TYPE_NUMERIC:
+						System.out.print(cell.getNumericCellValue() + "\t\t\t");
+						break;
+					default:
+					}
+				}
+				System.out.println("");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
